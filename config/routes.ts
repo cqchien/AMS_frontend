8f000d6d240334d9ba7b6a76d059b790bf05d980
cqchien <caochientp1@gmail.com>
2021-05-10 23:51:40 +0700

    config route

--- config/routes.ts
+++ config/routes.ts
@@ -1,70 +1,76 @@
 ﻿export default [
+
   {
     path: '/',
-    component: '../layouts/BlankLayout',
+    component: '../layouts/AdminLayout',
     routes: [
       {
-        path: '/user',
-        component: '../layouts/UserLayout',
-        routes: [
-          {
-            name: 'login',
-            path: '/user/login',
-            component: './User/login',
-          },
-        ],
-      },
-      {
+        name: 'Sign In',
         path: '/',
-        component: '../layouts/SecurityLayout',
-        routes: [
-          {
-            path: '/',
-            component: '../layouts/BasicLayout',
-            authority: ['admin', 'user'],
-            routes: [
-              {
-                path: '/',
-                redirect: '/welcome',
-              },
-              {
-                path: '/welcome',
-                name: 'welcome',
-                icon: 'smile',
-                component: './Welcome',
-              },
-              {
-                path: '/admin',
-                name: 'admin',
-                icon: 'crown',
-                component: './Admin',
-                authority: ['admin'],
-                routes: [
-                  {
-                    path: '/admin/sub-page',
-                    name: 'sub-page',
-                    icon: 'smile',
-                    component: './Welcome',
-                    authority: ['admin'],
-                  },
-                ],
-              },
-              {
-                name: 'list.table-list',
-                icon: 'table',
-                path: '/list',
-                component: './TableList',
-              },
-              {
-                component: './404',
-              },
-            ],
-          },
-          {
-            component: './404',
-          },
-        ],
-      },
+        component: './SignIn',
+      }
+      // {
+      //   path: '/user',
+      //   component: '../layouts/UserLayout',
+      //   routes: [
+      //     {
+      //       name: 'login',
+      //       path: '/user/login',
+      //       component: './User/login',
+      //     },
+      //   ],
+      // },
+      // {
+      //   path: '/',
+      //   component: '../layouts/SecurityLayout',
+      //   routes: [
+      //     {
+      //       path: '/',
+      //       component: '../layouts/BasicLayout',
+      //       authority: ['admin', 'user'],
+      //       routes: [
+      //         {
+      //           path: '/',
+      //           redirect: '/welcome',
+      //         },
+      //         {
+      //           path: '/welcome',
+      //           name: 'welcome',
+      //           icon: 'smile',
+      //           component: './Welcome',
+      //         },
+      //         {
+      //           path: '/admin',
+      //           name: 'admin',
+      //           icon: 'crown',
+      //           component: './Admin',
+      //           authority: ['admin'],
+      //           routes: [
+      //             {
+      //               path: '/admin/sub-page',
+      //               name: 'sub-page',
+      //               icon: 'smile',
+      //               component: './Welcome',
+      //               authority: ['admin'],
+      //             },
+      //           ],
+      //         },
+      //         {
+      //           name: 'list.table-list',
+      //           icon: 'table',
+      //           path: '/list',
+      //           component: './TableList',
+      //         },
+      //         {
+      //           component: './404',
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       component: './404',
+      //     },
+      //   ],
+      // },
     ],
   },
   {
