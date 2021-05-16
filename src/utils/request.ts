3a38bf3a26c0846cbec96275d0dd7fc73240834c
cqchien <caochientp1@gmail.com>
2021-05-16 22:18:33 +0700

    config respone http status code

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,22 +2,22 @@ import { extend } from 'umi-request';
 import { notification } from 'antd';
 import config from '../../config/api'
 
-const codeMessage: { [status: number]: string } = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
+const codeMessage: Record<number, string> = {
+  200:'The server successfully returned the requested data.',
+  201:'Create or modify data successfully. ',
+  202:'A request has entered the background queue (asynchronous task). ',
+  204:'Delete data successfully. ',
+  400:'The request sent has an error, and the server has not performed any new or modified data operations. ',
+  401:'The user does not have permission (the token, username, password are wrong). ',
+  403:'The user is authorized, but access is forbidden. ',
+  404:'The request is for a record that does not exist, and the server is not operating. ',
+  406:'The requested format is not available. ',
+  410:'The requested resource has been permanently deleted and will no longer be available. ',
+  422:'When creating an object, a validation error occurred. ',
+  500:'An error occurred in the server, please check the server. ',
+  502:'Gateway error. ',
+  503:'The service is unavailable, the server is temporarily overloaded or maintained. ',
+  504:'The gateway has timed out. ',
 };
 
 /** Exception handler */
@@ -25,10 +25,10 @@ const errorHandler = (error: { response: Response }): Response => {
   const { response } = error;
   if (response && response.status) {
     const errorText = codeMessage[response.status] || response.statusText;
-    const { status, url } = response;
+    const { status } = response;
 
     notification.error({
-      message: `Request error ${status}: ${url}`,
+      message: `Request error ${status}`,
       description: errorText,
     });
   } else if (!response) {
